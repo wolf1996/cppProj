ede7708e1cce371c802cecd41e7aaab9b6482e5d
wolf1996 <ksgiv37@gmail.com>
2017-01-19 15:47:29 +0300

    Добавлены "фишки"

--- GameMaster/primitives/player.cpp
+++ GameMaster/primitives/player.cpp
@@ -1,6 +1,6 @@
 #include "player.h"
 
-Player::Player()
+Player::Player():name_(std::string(""))
 {
 
 }
@@ -8,4 +8,12 @@ const char* Player::GetTest(const std::string& name){
     std::cout<< "call player_test with value: " << name << std::endl;
     std::string var = name;
    return "var";
+}
+
+void Player::GetChip(Field* field, const std::string& name){
+    chips_[name] = field->GetChip(name_);
+}
+
+void Player::SetName(const std::string& name){
+    name_ = name;
 }
