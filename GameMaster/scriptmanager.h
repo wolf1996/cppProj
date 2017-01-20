37e011f78b85b62042a01515c1d45a2fcf4af0a1
pacman29 <balyakin.d.v@gmail.com>
2017-01-20 16:49:07 +0300

    добавлена регистрация классов в скрипт

--- GameMaster/scriptmanager.h
+++ GameMaster/scriptmanager.h
@@ -4,11 +4,13 @@
 #include <memory>
 #include <sol.hpp>
 #include "configfile.h"
+#include <primitives/all.h>
 
 class ScriptManager
 {
     std::shared_ptr<ConfigFile> config_;
     sol::state lua;
+    sol::table namespace_;
 public:
     ScriptManager(const std::shared_ptr<ConfigFile>& config);
     void LoadObject(void);
