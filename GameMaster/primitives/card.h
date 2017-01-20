95f46c03042c691c8ed30b7df5718703312fd17d
wolf1996 <ksgiv37@gmail.com>
2017-01-20 15:39:41 +0300

    Добавлен Boost

--- GameMaster/primitives/card.h
+++ GameMaster/primitives/card.h
@@ -1,11 +1,15 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include "boost/filesystem/path.hpp"
 
 class Card
 {
 public:
     Card();
+private:
+    boost::filesystem::path face;
+    boost::filesystem::path back;
 };
 
 #endif // CARD_H
